0b9a0e069aa009c3818fe22a58a7c7671dccf4b0
Moose <macboookpro@Moosebook.local>
2013-07-25 22:57:33 +0200

    Initial Commit

---
+++ MoofBezierLibrary/MoofBezierLibrary.h
@@ -0,0 +1,13 @@
+//
+//  MoofBezierLibrary.h
+//  MoofBezierLibrary
+//
+//  Created by Moose on 7/25/13.
+//  Copyright (c) 2013 MooseFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoofBezierLibrary : NSObject
+
+@end
---
+++ MoofBezierLibrary/MoofBezierLibrary.m
@@ -0,0 +1,13 @@
+//
+//  MoofBezierLibrary.m
+//  MoofBezierLibrary
+//
+//  Created by Moose on 7/25/13.
+//  Copyright (c) 2013 MooseFactory. All rights reserved.
+//
+
+#import "MoofBezierLibrary.h"
+
+@implementation MoofBezierLibrary
+
+@end
